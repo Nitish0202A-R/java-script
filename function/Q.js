beaec15875d54bc95273fc2a2687ac426b55ee52
Nitish KR Singh <nitishsingh2148@gmail.com>
2025-12-13 20:57:57 +0530

    first commit

---
+++ function/Q.js
@@ -0,0 +1,34 @@
+// let marks={
+//     Nitish:90,
+//     Rohit:80,
+//     Rahul:70,
+//     Abhi:75,
+// }
+// for(let key in marks){
+//     console.log(("The marks of "+ key + " are " + marks[key]));
+// }
+
+// let marks={
+//      Nitish:90,
+//     Rohit:80,
+//     Rahul:70,
+//     Abhi:75
+// }
+//    console.log("Nitish marks are "+ marks.Nitish);
+// let keys = Object.keys(marks);
+
+// for (let i = 0; i < keys.length; i++) 
+//     console.log("The marks of " + keys[i] + " are " + marks[keys[i]]);
+// for(let  key in marks){
+//     console.log(marks[key]);
+// }
+
+
+let cn = 26;
+let i = 26;
+
+while (i != cn) {
+    i = 20;
+}
+
+console.log("You have entered the correct number");
--- function/functionCode.js
+++ function/functionCode.js
@@ -1,34 +1,34 @@
 // 👉 Step 1: Function Define (Banana)
 function averageOfTwoNumbers(number1, number2) {
     
-    // 👉 Step 2: Dono numbers ka sum nikalo
+    //  Step 2: Dono numbers ka sum nikalo
     let sum = number1 + number2;
 
-    // 👉 Step 3: Average ka formula
-    let average = sum / 2;
+    //  Step 3: Average ka formula
+    let average = sum / 2
 
-    // 👉 Step 4: Ek message banake return kar do
+    //  Step 4: Ek message banake return kar do
     let resultMessage = 
         "Numbers: " + number1 + " and " + number2 + 
         "\nSum = " + sum + 
         "\nAverage = " + average;
 
-    return resultMessage;  // 👈 return output
+    return resultMessage;  //  output
 }
 
-// 👉 Step 5: Variables declare
+// Step 5: Variables declare
 let A = 2;
 let B = 5;
 let C = 6;
 
-// 👉 Step 6: Function ko call karna
+//  Step 6: Function ko call karna
 console.log("---- A & B ----");
-console.log(averageOfTwoNumbers(A, B));
+console.log(averageOfTwoNumbers(A, B))
 console.log("\n");
 
 console.log("---- B & C ----");
-console.log(averageOfTwoNumbers(B, C));
+console.log(averageOfTwoNumbers(B, C))
 console.log("\n");
 
 console.log("---- C & A ----");
-console.log(averageOfTwoNumbers(C, A));
+console.log(averageOfTwoNumbers(C, A))
