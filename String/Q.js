1564b860919e6ff55124988a3603c44279a0782c
Nitish KR Singh <nitishsingh2148@gmail.com>
2025-12-15 00:18:36 +0530

    first commit

---
+++ String/Q.js
@@ -0,0 +1,20 @@
+//Q1
+let name="nitish \""
+console.log(name.length)
+//Q3
+console.log(name.toLowerCase())
+//Q4
+// let amount="please give to 1000"
+// console.log(amount.slice(14))
+
+let amount = "please give to 1000"
+// amount = Number.parseInt(amount.slice(14))
+
+console.log(amount)
+console.log(typeof amount)
+
+//Q5
+let friend="abhishek"
+friend[3]="s"
+console.log(friend)  // it is not changing because string are immutable in js
+
--- String/concat.js
+++ String/concat.js
@@ -1,3 +1,18 @@
-let name="nitish"
-let friend="abhishek"
- console.log(name.concat(" is friend of ",friend,))
+// let name="nitish"
+// let friend="abhishek"
+//  console.log(name.concat(" is friend of ",friend,))
+
+//  let friend2 ="   pradhan    "
+//  console.log(friend2.trim())
+
+//  let friend2 ="   pradhan    "
+//  console.log(friend2)
+
+let fr="nitish"+"abhishek"+"ashish"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr[4])
+console.log(fr[5])
+console.log(fr)
