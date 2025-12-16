859524290dc68bf59840204cf4dec7f00ad36203
Nitish KR Singh <nitishsingh2148@gmail.com>
2025-12-16 23:55:31 +0530

    first commit

---
+++ Arrays/loop.js
@@ -0,0 +1,13 @@
+// for loop in array
+let num=[25,26,27,28,29]
+// for(let i=0;i<num.length;i++){// num.ka shat hamsa lenght ka usa kra ha 
+//     console.log(num[i])
+// }
+// forEach loop
+num.forEach((Element)=>{ 
+    console.log(Element+Element) // Agar tum element * element print karte ho, to har number ka square print hoga
+})
+
+// araya from loop 
+let name="nitish"
+let naamclear
