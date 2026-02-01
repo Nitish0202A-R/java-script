fa19f0a8b6a839d91ddd2e1d7329d597ce3a325f
Nitish Kumar <nitishsingh2148@gmail.com>
2026-02-02 03:33:44 +0530

    first commit

--- function/function.js
+++ function/function.js
@@ -1,41 +1,75 @@
-// let A=2
-// let B=5
-// let C=6
-// console.log("sum number aur print A and B=",A+B)
-// console.log("sum number aur print B and C=",B+C)
-// console.log ("sum number aur print C and A=",C+A)
+// // let A=2
+// // let B=5
+// // let C=6
+// // console.log("sum number aur print A and B=",A+B)
+// // console.log("sum number aur print B and C=",B+C)
+// // console.log ("sum number aur print C and A=",C+A)
+
+// // function averger(A, B) {
+// //     console.log("done")
+// //     return (A + B) / 2;
+// // }
+
+// // let A = 2;
+// // let B = 5;
+// // let C = 6;
+
+// // console.log("averger number aur print A and B =", averger(A, B))
+// // console.log("averger number aur print B and C =", averger(B, C))
+// // console.log("averger number aur print C and A =", averger(C, A))
+
+// // function sum(a, b) {
+// //   return a + b;
+// // }
+
+// // console.log(sum(5,5));
+// // console.log(sum(10,5));
 
 // function averger(A, B) {
-//     console.log("done")
-//     return (A + B) / 2;
+//     return Math.round(1+ (A + B) / 2)
 // }
 
-// let A = 2;
-// let B = 5;
-// let C = 6;
+// let A = 1;
+// let B = 2;
+// let C = 3;
 
-// console.log("averger number aur print A and B =", averger(A, B))
-// console.log("averger number aur print B and C =", averger(B, C))
-// console.log("averger number aur print C and A =", averger(C, A))
+// console.log("averger number aur print A and B =", averger(A, B));
+// console.log("averger number aur print B and C =", averger(B, C));
+// console.log("averger number aur print C and A =", averger(C, A));
 
+// function hello (){
+//     console.log("object");
+// }
 
-// function sum(a, b) {
+// hello();
+
+// function greetUser(name) {
+//   console.log("Hello " + name);
+// }
+
+// greetUser("world");
+// greetUser("Rahul");
+
+// greetUser("nitish");
+
+// function add(a, b) {
+//  return a + b;
+// }
+
+// let result = add(5, 3);
+// console.log(result);
+
+// ARROW FUNCTION =
+
+// const Add = (a, b) => {
 //   return a + b;
-// }
+// };
 
-// console.log(sum(5,5));
-// console.log(sum(10,5));
+// console.log(Add(2, 5));
 
-
-function averger(A, B) {
-    return Math.round(1+ (A + B) / 2)
+function name(){
+    console.log("kumar")
 }
 
-let A = 1;
-let B = 2;
-let C = 3;
 
-console.log("averger number aur print A and B =", averger(A, B));
-console.log("averger number aur print B and C =", averger(B, C));
-console.log("averger number aur print C and A =", averger(C, A));
-
+name();
