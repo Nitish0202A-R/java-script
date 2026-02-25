2e82e2de2eebcb340f3697c2a7185cc4eab0397a
Nitish Kumar <nitishsingh2148@gmail.com>
2026-02-26 00:38:24 +0530

    first commit

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -83,5 +83,94 @@
 // Q 3️⃣ ["a","b","c"] → sabko uppercase karo
 
 
-const arr=["a","b","c"]
-console(uppercase(arr))
+// const arr=["a","b","c"]
+// const ans=arr.map(function(letter){
+//     return letter.toUpperCase()
+// })
+// console.log(ans)
+
+// Q Q5️⃣ Numbers ka square karo
+
+
+
+// const sq=[2,3,4]
+// const ans=sq.map((number)=>{
+//     return number*number
+// })
+// console.log(ans)
+
+
+// Q4️⃣ Names ke aage "Mr " add karo
+
+
+// const add=["nitish","rohit"]
+// const ans=add.map(function(n){
+//     return " Mr " + n;
+// })
+// console.log(ans)
+
+
+// const muli=[1,2,3,4]
+// const ans =muli.map((n)=>{
+//     return n*3
+// })
+// console.log(ans)
+
+
+
+
+// Q1️⃣
+
+// 👉 [1,5,8,2,10]
+// 👉 sirf >5 wale numbers lo
+
+
+// const less=[1,5,8,2,10]
+// const ans=less.filter((number)=>{
+//     return number >5
+// })
+
+// console.log(ans)
+
+
+// Q2️⃣
+
+// 👉 [10,20,3,7,50]
+// 👉 sirf <15 wale numbers lo
+
+
+// const ge=[10,20,3,7,50]
+// const ans=ge.filter((number)=>{
+//     return number<15
+// })
+// console.log(ans)
+
+
+
+// Q3️⃣
+
+// 👉 [2,4,5,7,8,9]
+// 👉 sirf even numbers lo
+
+
+// const even= [2,4,5,7,8,9]
+// const ans=even.filter((number)=>{
+//     return number%2===0
+// })
+
+// console.log(ans)
+
+
+
+// Q4️⃣
+
+// 👉 [15,30,4,9,100]
+// 👉 sirf 10 se bade numbers lo
+
+
+const num=[15,30,4,9,100]
+const ans=num.filter((number)=>{
+    return number>10
+})
+
+console.log(ans)
